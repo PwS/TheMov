1196299c6f814c63d4326b50867cebd312c6f320
PwS <pawanasutha02@gmail.com>
2023-07-04 21:59:55 +0700

    Add OnBoardingPage

---
+++ app/src/main/java/com/pws/themov/onboarding/OnBoardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.pws.themov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pws.themov.R
+
+class OnBoardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_three)
+    }
+}
