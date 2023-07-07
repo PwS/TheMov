af346d08f92496e04094de7375a69cfac40ec324
PwS <pawanasutha02@gmail.com>
2023-07-07 23:58:06 +0700

    Add Activity Signup & Signup photo

---
+++ app/src/main/java/com/pws/themov/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.pws.themov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
