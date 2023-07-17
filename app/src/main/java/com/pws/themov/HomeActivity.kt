3098c806dc579f9f2d955f82c8f9ef9d4f1b7aa5
PwS <pawanasutha02@gmail.com>
2023-07-17 21:05:32 +0700

    Add Binding

--- app/src/main/java/com/pws/themov/HomeActivity.kt
+++ app/src/main/java/com/pws/themov/HomeActivity.kt
@@ -1,11 +1,16 @@
 package com.pws.themov
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pws.themov.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/pws/themov/SplashScreenActivity.kt
+++ app/src/main/java/com/pws/themov/SplashScreenActivity.kt
@@ -1,15 +1,19 @@
 package com.pws.themov
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.pws.themov.databinding.ActivitySplashScreenBinding
 import com.pws.themov.onboarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val handler = Handler()
 
