c27394c0f062c39f24464d7a0be41d6cfaff17ae
PwS <pawanasutha02@gmail.com>
2023-07-21 20:53:41 +0700

    Add Binding

--- app/src/main/java/com/pws/themov/profile/EditProfileActivity.kt
+++ app/src/main/java/com/pws/themov/profile/EditProfileActivity.kt
@@ -1,14 +1,15 @@
 package com.pws.themov.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pws.themov.R
+import androidx.appcompat.app.AppCompatActivity
+import com.pws.themov.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
