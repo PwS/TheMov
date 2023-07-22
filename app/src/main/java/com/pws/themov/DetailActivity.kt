5e9640ce8e8a4a371c1123e1ae3e6e1606a7a5bf
PwS <pawanasutha02@gmail.com>
2023-07-22 18:22:57 +0700

    Add Binding

--- app/src/main/java/com/pws/themov/DetailActivity.kt
+++ app/src/main/java/com/pws/themov/DetailActivity.kt
@@ -1,11 +1,15 @@
 package com.pws.themov
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pws.themov.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
